bf1103c4c1fb6cf3157eeb07f136daf9b8f034d7
jmartin <jason.martin@infor.com>
2024-06-25 13:48:34 -0600

    added pull aws accounts

---
+++ src/pull_aws_accounts.py
@@ -0,0 +1,25 @@
+import boto3
+import botocore.exceptions
+
+
+def list_accounts(credentials, logger):
+    account_list = []
+    access_key = credentials['AccessKeyId']
+    secret_access_key = credentials['SecretAccessKey']
+    session_key = credentials['SessionToken']
+    try:
+        org_client = boto3.client('organizations',
+                                  aws_access_key_id=access_key,
+                                  aws_secret_access_key=secret_access_key,
+                                  aws_session_token=session_key
+                                  )
+
+        paginator = org_client.get_paginator('list_accounts')
+        page_iterator = paginator.paginate()
+        for page in page_iterator:
+            for acct in page['Accounts']:
+                account_id = acct['Id']
+                account_list.append(account_id)
+        return account_list
+    except botocore.exceptions.ClientError as error:
+        logger.critical(f'error retrieving accounts {error}')
